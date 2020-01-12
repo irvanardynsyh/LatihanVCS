fc904f1ad7151cc2e97352f42180605e4c67514e
irvanardynsyh <56512562+irvanardynsyh@users.noreply.github.com>
2020-01-12 17:17:24 +0700

    Project

---
+++ Project/Lab 2.py
@@ -0,0 +1,12 @@
+a=input("Masukan nilai a : ")
+b=input("Masukan nilai b : ")
+print("Variable a = ", a)
+print("Variable b = ", b)
+print("Hasil penggabungan {0} & {1} = %a".format(a,b) %(a+b))
+
+
+#Konversi Nilai Variable
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan {0} + {1} = %d".format(a,b) %(a+b))
+print("Hasil pembagian {0} / {1} = %d".format(a,b) %(a/b))
